726a1ce776e9dfa41ea66ca98445b7d8ae709f95
Bhaaskar Bolloju <bhaaskarbolloju@gmail.com>
2018-07-05 16:51:22 +0530

    Initial changes

---
+++ src/TestClass.java
@@ -0,0 +1,13 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is a sample test class");
+		System.out.println("Modified something");
+		
+		
+		
+	}
+
+}
